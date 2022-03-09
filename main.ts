0c29d5d277bd96ea1f15a116423a5699c1700091
Alan/Jed <obvicechief@gmail.com>
2022-03-09 10:24:09 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("THE HEAVY IS DEAD")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
